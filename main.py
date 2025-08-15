eb816c30c03ab432460596aa9bdf0a959a577a49
carlosvarxc <136493271+carlosvarxc@users.noreply.github.com>
2025-08-15 10:44:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 import secrets
 
 projects = ["Project A", "Project B", "Project C", "Project D"]
-print("Dự án ngẫu nhiên:", secrets.choice(projects))--06--10--11--13--14
+print("Dự án ngẫu nhiên:", secrets.choice(projects))--06--10--11--13--14--15
